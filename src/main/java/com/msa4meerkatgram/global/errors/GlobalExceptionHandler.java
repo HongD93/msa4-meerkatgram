1869f41a40b294a94d08ee5d548b750ad50a1e71
HongD93 <tigerson93@gmail.com>
2026-05-27 17:47:18 +0900

    260527 [develop] JwtProvider, 로그인 작업중

--- src/main/java/com/msa4meerkatgram/global/errors/GlobalExceptionHandler.java
+++ src/main/java/com/msa4meerkatgram/global/errors/GlobalExceptionHandler.java
@@ -1,9 +1,9 @@
 package com.msa4meerkatgram.global.errors;
 
+import com.msa4meerkatgram.global.errors.custom.NotRegisteredException;
 import com.msa4meerkatgram.global.responses.GlobalRes;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,13 +15,24 @@ import java.util.List;
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(NotRegisteredException.class)
+    public ResponseEntity<GlobalRes<String>> notRegisteredHandle(NotRegisteredException e) {
+        return ResponseEntity.status(400).body(
+                GlobalRes.<String>builder()
+                        .code("E01")
+                        .message("로그인 에러")
+                        .data(e.getMessage())
+                        .build()
+        );
+    }
+
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<GlobalRes<String>> methodArgumentTypeMismatchHandle(MethodArgumentTypeMismatchException e) {
         return ResponseEntity.status(400).body(
                 GlobalRes.<String>builder()
                         .code("E21")
-                        .message("요청 파라미터에 이상이 있습니다")
-                        .data(String.format("%s : 필드를 확인해주세요.", e.getName()))
+                        .message("요청 파라미터에 이상이 있습니다.")
+                        .data(String.format("%s : 필드를 확인해 주세요.", e.getName()))
                         .build()
         );
     }
@@ -31,13 +42,13 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(400).body(
                 GlobalRes.<List<String>>builder()
                         .code("E21")
-                        .message("요청 파라미터에 이상이 있습니다")
+                        .message("요청 파라미터에 이상이 있습니다.")
                         .data(
                                 e.getBindingResult()
-                                .getAllErrors()
-                                .stream()
-                                .map(ObjectError::getDefaultMessage)
-                                .toList()
+                                        .getAllErrors()
+                                        .stream()
+                                        .map(item -> String.format("%s : 잘못된 값입니다.", item.getObjectName()))
+                                        .toList()
                         )
                         .build()
         );
@@ -45,7 +56,12 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<GlobalRes<String>> othersHandle(Exception e) {
-        log.error(String.format("시스템 에러: %s\n%s", e.getMessage(), Arrays.toString(e.getStackTrace())));
+        log.error(String.format(
+                        "시스템 에러: %s\n%s"
+                        ,e.getMessage()
+                        ,Arrays.toString(e.getStackTrace())
+                )
+        );
         return ResponseEntity.status(500).body(
                 GlobalRes.<String>builder()
                         .code("E99")
