b6df77b274065e5d85b06d4fb2c57bf08d8370d7
HongD93 <tigerson93@gmail.com>
2026-05-29 16:52:38 +0900

    260529 [develop] 로그인 작업 완료, 리이슈 작업 완료

--- src/main/java/com/msa4meerkatgram/global/errors/GlobalExceptionHandler.java
+++ src/main/java/com/msa4meerkatgram/global/errors/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.msa4meerkatgram.global.errors;
 
+import com.msa4meerkatgram.global.errors.custom.InvalidTokenException;
 import com.msa4meerkatgram.global.errors.custom.NotRegisteredException;
 import com.msa4meerkatgram.global.responses.GlobalRes;
 import lombok.extern.slf4j.Slf4j;
@@ -15,11 +16,22 @@ import java.util.List;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(NotRegisteredException.class)
-    public ResponseEntity<GlobalRes<String>> NotRegeistereHandle(NotRegisteredException e) {
+    public ResponseEntity<GlobalRes<String>> NotRegisterHandle(NotRegisteredException e) {
         return ResponseEntity.status(400).body(
                 GlobalRes.<String>builder()
                         .code("E01")
                         .message("로그인 에러")
+                        .data(e.getMessage())
+                        .build()
+        );
+    }
+
+    @ExceptionHandler(InvalidTokenException.class)
+    public ResponseEntity<GlobalRes<String>> invalidTokenHandle(InvalidTokenException e) {
+        return ResponseEntity.status(400).body(
+                GlobalRes.<String>builder()
+                        .code("E04")
+                        .message("토큰 이상")
                         .data(e.getMessage())
                         .build()
         );
